4cd4a942e886997c6963c87f58c1d05d31436710
Georgiana Rontu <rontugeorgiana@gmail.com>
2022-09-02 18:59:38 +0300

    new endpoint

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post, UseFilters } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseFilters } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 import { ApiTags } from '@nestjs/swagger';
+import { UserParam } from 'src/common/decorators/user.decorator';
 import { CreateUserDto } from '../dtos/create-user.dto';
+import { User } from '../entities/user.entity';
 import { UsersService } from '../providers/users.service';
 
 @UseFilters(BaseExceptionFilter)
@@ -13,5 +15,10 @@ export class UsersController {
   @Post('/create-user')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.addUser(createUserDto);
+  }
+
+  @Get('/details')
+  getUserDetails(@UserParam() user: User) {
+    return this.usersService.getMinimalUserById(user.id);
   }
 }
